9793fedc88004d42bc0ece07dfc7e9d10038880b
Ian Kim <ian@iankim.dev>
2025-02-09 22:34:51 -0600

    init

---
+++ home-manager/home.nix
@@ -0,0 +1,219 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: let 
+  vimrcRepo = builtins.fetchGit {
+      url = "https://github.com/ianmkim/vimrc";
+      rev = "55370c21bd2b2d304d0665df3e06c5c5e210cf25"; 
+      submodules = true;
+    };
+in {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    # outputs.homeManagerModules.example
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  # TODO: Set your username
+  home = {
+    username = "adrian";
+    homeDirectory = "/home/adrian";
+  };
+
+  # Add stuff for your user as you see fit:
+  # programs.neovim.enable = true;
+  home.packages = with pkgs; [ 
+    steam 
+    ibm-plex
+    tmux
+    ack
+    lazygit
+    python312Full
+    python312Packages.pip
+    rustup
+    gcc
+  ];
+
+  home.sessionVariables = {
+      TERMINFO_DIRS = "${pkgs.kitty.terminfo.outPath}/share/terminfo";
+  };
+  
+  # Add Vim and amix/vimrc configuration
+  programs.vim.packageConfigurable = {
+    enable = true;
+  };
+
+  programs.kitty = {
+      enable = true;
+  };
+
+  # Fetch amix/vimrc repository
+  # Clone amix/vimrc repository into ~/.vim_runtime
+  home.file.".vim_runtime".source = vimrcRepo;
+
+  # Set up .vimrc to source amix/vimrc
+  home.file.".vimrc".text = ''
+    set runtimepath+=~/.vim_runtime
+    source ~/.vim_runtime/vimrcs/basic.vim
+    source ~/.vim_runtime/vimrcs/filetypes.vim
+    source ~/.vim_runtime/vimrcs/plugins_config.vim
+    source ~/.vim_runtime/vimrcs/extended.vim
+  '';
+
+
+  # add shell configuration
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+
+    oh-my-zsh = {
+       enable = true;
+      plugins = ["git"];
+      theme = "candy";
+    };
+
+    shellAliases = {
+      ll = "ls -l";
+    };
+  };
+
+  # add tmux configuration
+  programs.tmux = {
+      enable = true;
+      extraConfig = ''
+      set -g default-terminal "screen-256color"
+
+      # turn on mouse support
+      set-option -g mouse on
+      set -g @scroll-speed-num-lines-per-scroll "1"
+
+      # update prefix hotkey
+      set-option -g prefix C-a
+      bind-key C-a send-prefix
+
+      # update pane-split keys
+      bind | split-window -h
+      bind - split-window -v
+      unbind '"'
+      unbind %
+
+      # update pane switching
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+
+      # update alternate pane switching with Alt
+      # for mac, preference > profile > keyboard
+      # and use Option as Meta key
+      bind -n M-h select-pane -L
+      bind -n M-j select-pane -D
+      bind -n M-k select-pane -U
+      bind -n M-l select-pane -R
+
+      # reload config file
+      bind r source-file ~/.tmux.conf \; display "reloaded tmux configuration"
+
+      # shorten command delay
+      set -g escape-time 1
+
+      # set window and pane base index to 1
+      set-option -g base-index 1
+      setw -g pane-base-index 1
+
+      # make the current window first window
+      bind T swap-window -t 1
+
+      # update history to 10k
+      set -g history-limit 10000
+
+      # swap windows
+      bind -r > swap-window -t +1
+      bind -r < swap-window -t -1
+
+      # switch windows
+      bind -r ] select-window -t :+
+      bind -r [ select-window -t :-
+
+      ## Theme (ix.i0/V5C)
+
+      # panes
+      set -g pane-border-style fg=black
+      set -g pane-active-border-style fg=colour8
+
+      # toggle statusbar
+      bind-key b set-option status
+
+      # status line
+      set -g status-justify left
+      set -g status-bg colour16
+      set -g status-fg colour16
+      set -g status-interval 2
+      
+      # messaging
+      set -g automatic-rename on
+
+      # colors
+      setw -g window-status-format "#[fg=colour3] •#[fg=colour8] #W "
+      setw -g window-status-current-format "#[fg=colour2] •#[fg=colour7] #W "
+      set -g status-position bottom
+      set -g status-justify centre
+      set -g status-left "  #[fg=colour3]• #[fg=colour2]• #[fg=colour4]•"
+      set -g status-right " #[fg=colour4] •#[fg=colour8] #S  "
+
+
+      # plugins
+      set -g @plugin 'tmux-plugins/tmux-copycat'
+
+      # initialize plugin manager
+      run '~/.tmux/plugins/tpm/tpm'
+      '';
+  };
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,159 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "rome"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # enable battery optimizations
+  services.power-profiles-daemon.enable = false;
+  powerManagement.enable = true;
+  powerManagement.powertop.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 50;
+
+    };
+  };
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # set default shell as zsh
+  programs.zsh.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.adrian = {
+    isNormalUser = true;
+    description = "adrian";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # allow nix-command and flakes
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+     google-chrome
+     wget
+     git
+     zsh
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+   services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
